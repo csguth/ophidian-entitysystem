5e817c3e152558865159ddb00d0c5fbd3b032941
Chrystian de Sousa Guth <csguth@gmail.com>
2016-07-29 18:38:15 -0300

    creating netlist

--- ophidian/netlist/netlist.h
+++ ophidian/netlist/netlist.h
@@ -1,14 +1,53 @@
 #ifndef OPHIDIAN_NETLIST_NETLIST_H
 #define OPHIDIAN_NETLIST_NETLIST_H
 
+#include <ophidian/entity_system/entity_system.h>
+
 namespace ophidian {
 namespace netlist {
 
+class netlist;
+
+#define CREATE_OPAQUE_ENTITY(NAME) \
+    class NAME {\
+        entity_system::entity_system::entity m_entity;\
+        friend class netlist;\
+        NAME(entity_system::entity_system::entity entity) : m_entity(entity) {}\
+    public:\
+        NAME(void) {}\
+    };
+
+
+#define CREATE_ENTITY_HANDLING_METHODS(NAME, SYSTEM) \
+    template <> NAME create(void){ return NAME(SYSTEM.create()); }
+//\
+//    template <> void netlist::destroy<NAME>(NAME en){ SYSTEM.destroy(en.m_entity); }\
+//    template <> bool netlist::valid<NAME>(NAME en) const { return SYSTEM.valid(en.m_entity); }\
+//    template <> std::size_t netlist::size<NAME>(void) const { return SYSTEM.size(); }
+
+
+CREATE_OPAQUE_ENTITY(cell)
+CREATE_OPAQUE_ENTITY(pin)
+CREATE_OPAQUE_ENTITY(net)
+
+
 class netlist
 {
+    entity_system::entity_system m_cells;
+    entity_system::entity_system m_pins;
+    entity_system::entity_system m_nets;
 public:
     netlist();
+    CREATE_ENTITY_HANDLING_METHODS(cell, m_cells)
+    CREATE_ENTITY_HANDLING_METHODS(pin, m_pins)
+
+
+//    CREATE_ENTITY_HANDLING_METHODS(net, m_nets)
+
+
 };
+
+
 
 }
 }
--- test/netlist_test.cpp
+++ test/netlist_test.cpp
@@ -5,21 +5,69 @@
 using namespace ophidian;
 
 using Netlist = netlist::netlist;
+using Cell = netlist::cell;
+using Pin = netlist::pin;
 
 class BrandNewNetlist_Fixture {
     Netlist m_netlist;
-
 public:
-    const Netlist & netlist() const {
+    Netlist & netlist() {
         return m_netlist;
     }
 };
 
+class Netlist_With_One_Cell_Fixture : public BrandNewNetlist_Fixture {
+    Cell m_cell;
+public:
+    Netlist_With_One_Cell_Fixture() : m_cell(netlist().create<Cell>()) {
+
+    }
+
+    netlist::cell cell() const {
+        return m_cell;
+    }
+
+};
+
+class Netlist_With_One_Pin_Fixture : public BrandNewNetlist_Fixture {
+    Pin m_pin;
+public:
+    Netlist_With_One_Pin_Fixture() : m_pin(netlist().create<Pin>()) {
+
+    }
+
+    netlist::pin pin() const {
+        return m_pin;
+    }
+
+};
+
 TEST_CASE_METHOD(BrandNewNetlist_Fixture, "netlist: creation", "[netlist]") {
-//    REQUIRE(netlist().cells_size() == 0);
-//    REQUIRE(netlist().pins_size() == 0);
-//    REQUIRE(netlist().nets_size() == 0);
+    REQUIRE(netlist().size<Cell>() == 0);
+    REQUIRE(netlist().size<Pin>() == 0);
 }
+
+//TEST_CASE_METHOD(Netlist_With_One_Cell_Fixture, "netlist: create cell", "[netlist]") {
+//    REQUIRE(netlist().valid(cell()));
+//    REQUIRE(netlist().size<Cell>() == 1);
+//}
+
+//TEST_CASE_METHOD(Netlist_With_One_Cell_Fixture, "netlist: destroy cell", "[netlist]") {
+//    netlist().cell_destroy(cell());
+//    REQUIRE(netlist().size<Cell>() == 0);
+//}
+
+
+//TEST_CASE_METHOD(Netlist_With_One_Pin_Fixture, "netlist: create pin", "[netlist]") {
+//    REQUIRE(netlist().valid(pin()));
+//    REQUIRE(netlist().size<Pin>() == 1);
+//}
+
+//TEST_CASE_METHOD(Netlist_With_One_Pin_Fixture, "netlist: destroy pin", "[netlist]") {
+//    netlist().pin_destroy(pin());
+//    REQUIRE(netlist().size<Pin>() == 0);
+//}
+
 
 
 
